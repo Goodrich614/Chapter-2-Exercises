8cbaced6a1abc6d5924253261130ce3cf5f94521
Sgoody614 <sean.goodrich@clincierge.com>
2022-04-11 11:22:49 -0700

    Create fizzBuzz function

--- assignments.js
+++ assignments.js
@@ -1,15 +1,25 @@
 // Looping a triangle
 const buildTriangle = () => {
-/*
- * Statement 1: let row = "#" --- initializes the variable
- * Statement 2: row.length < 8 --- condition for the for loop to run (How many times, typically)
- * Statement 3: row += "#" --- typically used to increment the iteration. 
- *                In this case it adds '#' to the initial row value of '#' as long as the condition is true
- */
-for (let row = "#"; row.length < 8; row += "#")
-  console.log(row);
+    /*
+     * Statement 1: let row = "#" --- initializes the variable
+     * Statement 2: row.length < 8 --- condition for the for loop to run (How many times, typically)
+     * Statement 3: row += "#" --- typically used to increment the iteration. 
+     *                In this case it adds '#' to the initial row value of '#' as long as the condition is true
+     */
+    for (let row = "#"; row.length < 8; row += "#")
+        console.log(row);
 }
 
 buildTriangle();
 
-// 
+// Fizz Buzz
+const fizzBuzz = () => {
+    for (let num = 1; num <= 100; num++) {
+        if (num % 3 === 0) console.log('Fizz');
+        if (num % 5 === 0) console.log('Buzz');
+        console.log(num);
+    }
+}
+
+fizzBuzz();
+
